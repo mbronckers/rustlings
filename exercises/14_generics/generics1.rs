e3739d8877ea571b7a7a1c60b8a249ba2cd48310
Max Bronckers <maxbronckers@me.com>
2023-12-28 20:12:45 +0100

    complete generics

--- exercises/14_generics/generics1.rs
+++ exercises/14_generics/generics1.rs
@@ -6,9 +6,13 @@
 // Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
-    shopping_list.push("milk");
+    /* 
+        &'static str is a reference to a string slice with a 'static lifetime; guaranteed 
+        to be valid for the whole duration of the program.
+    */
+    let mut shopping_list: Vec<&'static str> = Vec::new(); 
+    // 'static is a lifetime specifier, often used for literals
+    shopping_list.push("milk"); // "milk" is a string slice
 }
