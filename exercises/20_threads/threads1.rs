2de97e5d00dda6966c6e1efaafd750cd217d9b72
Max Bronckers <maxbronckers@me.com>
2023-12-30 17:51:49 +0100

    complete threads

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -26,7 +26,8 @@ fn main() {
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        // Join: Wait for all threads to finish.
+        results.push(handle.join().unwrap());
     }
 
     if results.len() != 10 {
